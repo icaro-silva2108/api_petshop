18243c06488e3ef3b2e4ca157b3837af8c8b47eb
Icaro Silva <icarosilva00562@gmail.com>
2026-06-15 14:15:16 -0300

    add health endpoint permisson to SecurityConfig

--- src/main/java/com/icaro/api_petshop/config/SecurityConfig.java
+++ src/main/java/com/icaro/api_petshop/config/SecurityConfig.java
@@ -42,7 +42,8 @@ public class SecurityConfig {
 
                                 .requestMatchers(
                                         "/swagger-ui/**",
-                                        "/v3/api-docs/**"
+                                        "/v3/api-docs/**",
+                                        "/actuator/health"
                                 ).permitAll()
 
                                 .anyRequest().authenticated()
